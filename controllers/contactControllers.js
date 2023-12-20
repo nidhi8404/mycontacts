810180b2dda5a9fca3febe19e5a00000f48a6d54
nidhi8404 <nidhi2shine@gmail.com>
2023-12-21 02:12:39 +0530

    dbConnected

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -1,30 +1,48 @@
-const getContacts = (req , res) => {
-    res.status(200).json({message : "Get My Contacts"});
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+
+const getContacts = asyncHandler(async (req, res) => {
+  const contacts = await Contact.find();
+  res.status(200).json(contacts);
+});
+
+const createContact = asyncHandler(async (req, res) => {
+  console.log("The request body :", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All Fields Mandatory!");
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
+});
+
+const getContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact Not Found");
+  }
+  res.status(200).json(contact);
+});
+
+const updateContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById;
+  res.status(200).json({ message: `Update Contact for ${req.params.id}` });
+});
+
+const deleteContact = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
+});
+
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
 };
-
-const createContact = (req , res) => {
-    console.log("The request body :" , req.body);
-    const{name , email , phone} = req.body;
-    if(!name || !email || !phone){
-        res.status(400);
-        throw new Error ("All Fields Mandatory!"); 
-    }
-    res.status(201).json({message : "Create Contact"});
-};
-
-const getContact = (req , res) => {
-    res.status(200).json({message : `Find Contact for ${req.params.id}`});
-};
-
-const updateContact = (req , res) => {
-    res.status(200).json({message : `Update Contact for ${req.params.id}`});
-};
-
-const deleteContact = (req , res) => {
-    res.status(200).json({message : `Delete Contact for ${req.params.id}`});
-};
-
-module.exports = {getContacts, createContact , getContact, updateContact,deleteContact };
-
-
-
---
+++ models/contactModel.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true , "Please add ur name"]
+    },
+    email: {
+        type: String,
+        required: [true , "Please add ur email"]
+    },
+    phone: {
+        type: String,
+        required: [true , "Please add ur phone"]
+    },
+},
+
+{
+    timestamps: true,
+});
+
+module.exports = mongoose.model("Contact" , contactSchema);
